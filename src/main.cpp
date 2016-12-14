ef79b7240849373c54f8e2cf4819be63f01ad8b7
Paulo Brizolara <souzabrizolara@gmail.com>
2016-12-14 13:50:05 -0300

    Configuração inicial do projeto

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * Blink
+ *
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+#include "Arduino.h"
+
+#define LED_PIN 5
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_PIN, HIGH);
+
+  // wait for a second
+  delay(1000);
+
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_PIN, LOW);
+
+   // wait for a second
+  delay(1000);
+}
